d5e614d6d4b35a46805b6728aed1f743657b1c91
cgarciapetiet <cgarciapetiet@laba.solvd.com>
2025-10-07 13:31:07 -0300

    creation of the grid with hover behaviour

---
+++ script.js
@@ -0,0 +1,7 @@
+const sContainer= document.querySelector("#square-container");
+
+for(let i=0; i<256; i++){
+    const square= document.createElement("div");
+    square.classList.add("square");
+    sContainer.appendChild(square);
+}
